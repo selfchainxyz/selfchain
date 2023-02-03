242c9c3e3a8f223f2bd1ac99d97157c594aeffc4
ppoliani <ppoliani@gmail.com>
2023-02-03 13:12:31 +0200

    feat: check that signer is a migrator account

--- x/migration/keeper/msg_server_migrate.go
+++ x/migration/keeper/msg_server_migrate.go
@@ -11,15 +11,10 @@ import (
 func (k msgServer) Migrate(goCtx context.Context, msg *types.MsgMigrate) (*types.MsgMigrateResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	acl, found := k.GetAcl(ctx)
-	if !found {
-		panic("Acl not found")
-	}
-
-	_ = acl
-
 	// 1. Make sure signer is in the list of migrators
-
+	_, found := k.GetMigrator(ctx, msg.Creator); if !found {
+		return nil, types.ErrUnknownMigrator
+	}
 	// 2. Create a hash of the message
 
 	// 3. Check if message i.e. migration request has been processed already
--- x/migration/keeper/msg_server_test.go
+++ x/migration/keeper/msg_server_test.go
@@ -7,10 +7,15 @@ import (
 	keepertest "frontier/testutil/keeper"
 	"frontier/x/migration/keeper"
 	"frontier/x/migration/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
 	k, ctx := keepertest.MigrationKeeper(t)
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func TestShouldFailIfInvalidMigrator(t *testing.T) {
+	
 }
