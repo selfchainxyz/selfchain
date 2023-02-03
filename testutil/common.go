6a02f4f2e67b0c1523cfe011cab4162e35ddb28f
ppoliani <ppoliani@gmail.com>
2023-02-03 15:03:04 +0200

    chore: add test accounts

--- testutil/common.go
+++ testutil/common.go
@@ -4,6 +4,14 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+const (
+	Alice = "front1wnr2vnx8tjxv8mkp20dagdvn8pylr7x7l0mt9s"
+	Bob   = "front1kphjpt7crfmlhjyzvf44ng3ye9rvfy7z3pyajj"
+	Carol = "front1skzx07z9nhnv5js3x5ucchey7yyc6enk2l3sqx"
+	Migrator_1 = "front16077t285ajgp5a9retp4fkhs7enusr2n3lp4jg"
+	Migrator_2 = "front1wnr2vnx8tjxv8mkp20dagdvn8pylr7x7l0mt9s"
+)
+
 func InitSDKConfig() {
 	accountPubKeyPrefix := "frontpub"
 	validatorAddressPrefix := "frontvaloper"
@@ -17,3 +25,4 @@ func InitSDKConfig() {
 	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
 	config.Seal()
 }
+
--- x/migration/keeper/msg_server_test.go
+++ x/migration/keeper/msg_server_test.go
@@ -4,18 +4,26 @@ import (
 	"context"
 	"testing"
 
+	commontest "frontier/testutil"
 	keepertest "frontier/testutil/keeper"
+	"frontier/testutil/sample"
 	"frontier/x/migration/keeper"
 	"frontier/x/migration/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+func setup(t testing.TB) (types.MsgServer, context.Context) {
+	commontest.InitSDKConfig()
+
 	k, ctx := keepertest.MigrationKeeper(t)
+	
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
 }
 
 func TestShouldFailIfInvalidMigrator(t *testing.T) {
-	
+	// create a couple of migrators
+	setup(t)
+	migrator1 := sample.AccAddress()
+	migrator1 := sample.AccAddress()
 }
