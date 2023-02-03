94196472700d6382f769d041d96b53735b2349a9
ppoliani <ppoliani@gmail.com>
2023-02-03 10:56:12 +0200

    chore: outline the steps to implement the Migrate msg

--- x/migration/keeper/msg_server_migrate.go
+++ x/migration/keeper/msg_server_migrate.go
@@ -4,11 +4,27 @@ import (
 	"context"
 
 	"frontier/x/migration/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) Migrate(goCtx context.Context, msg *types.MsgMigrate) (*types.MsgMigrateResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	
+	acl, found := k.GetAcl(ctx); if !found {
+		panic("Acl not found")
+	}
+
+	_ = acl
+
+	// 1. Make sure signer is in the list of migrators
+
+
+	// 2. Create a hash of the message
+
+	// 3. Check if message i.e. migration request has been processed already
+
+	// 4. Mint new tokens to the destAddress
 
 	// TODO: Handling the message
 	_ = ctx
