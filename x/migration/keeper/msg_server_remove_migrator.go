0b0538cf0250407224bd64a959edfcbae0ef2da9
ppoliani <ppoliani@gmail.com>
2023-02-07 18:39:51 +0200

    feat: implement RemoveMigrator

--- x/migration/keeper/msg_server_remove_migrator.go
+++ x/migration/keeper/msg_server_remove_migrator.go
@@ -4,14 +4,23 @@ import (
 	"context"
 
 	"frontier/x/migration/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) RemoveMigrator(goCtx context.Context, msg *types.MsgRemoveMigrator) (*types.MsgRemoveMigratorResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	acl, aclExists := k.GetAcl(ctx)
+	if !aclExists {
+		panic("ACL does not exist")
+	}
+
+	if acl.Admin != msg.Creator {
+		return nil, types.ErrOnlyAdmin
+	}
+
+	k.Keeper.RemoveMigrator(ctx,  msg.Migrator);
 
 	return &types.MsgRemoveMigratorResponse{}, nil
 }
