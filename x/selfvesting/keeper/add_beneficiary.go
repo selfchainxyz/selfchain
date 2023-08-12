e3d824a00b287e8e32d53f0c87caba11c251be28
ppoliani <ppoliani@gmail.com>
2023-08-12 11:36:14 +0300

    feat: add basic add beneficiary funn structure

---
+++ x/selfvesting/keeper/add_beneficiary.go
@@ -0,0 +1,12 @@
+package keeper
+
+import (
+	"context"
+	"selfchain/x/selfvesting/types"
+)
+
+func (k Keeper) AddBeneficiary(goCtx context.Context) (*types.VestingInfo, error) {
+	// TODO: Implement
+
+	return &types.VestingInfo{}, nil
+}
