73540e45fec6ea75e1cce32bfa1c7879c4df6653
ppoliani <ppoliani@gmail.com>
2023-08-12 12:09:44 +0300

    feat: imlement add beneficiary

--- x/selfvesting/keeper/add_beneficiary.go
+++ x/selfvesting/keeper/add_beneficiary.go
@@ -3,10 +3,39 @@ package keeper
 import (
 	"context"
 	"selfchain/x/selfvesting/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-func (k Keeper) AddBeneficiary(goCtx context.Context) (*types.VestingInfo, error) {
-	// TODO: Implement
+func (k Keeper) AddBeneficiary(goCtx context.Context, req types.AddBeneficiaryRequest) (*types.VestingInfo, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	return &types.VestingInfo{}, nil
+	// check the benficiary address is a valid bech32 address
+	_, err := sdk.AccAddressFromBech32(req.Beneficiary)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid beneficiary address (%s)", err)
+	}
+
+	// create and add a new vesting position under the beneficiary key
+	vestingInfos := []*types.VestingInfo {}
+	vestingPositions, positionsExist := k.GetVestingPositions(ctx, req.Beneficiary)
+	
+	if !positionsExist {
+		vestingInfos = vestingPositions.VestingInfos
+	}
+
+	startTime := uint64(ctx.BlockHeader().Time.Unix())
+	newPosition := &types.VestingInfo{
+		StartTime:     startTime,
+		Duration:      req.Duration,
+		Cliff:         startTime + req.Cliff,
+		Amount:        req.Amount,
+		TotalClaimed:  0,
+		PeriodClaimed: 0,
+	}
+
+	vestingPositions.VestingInfos = append(vestingInfos, newPosition)
+	
+	return newPosition, nil
 }
