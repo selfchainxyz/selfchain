d9a1401190307366c5300e7f24f8ef31a09e0ecc
ppoliani <ppoliani@gmail.com>
2024-03-22 16:05:02 +0200

    fix: e2e test setup

--- app/test_helper.go
+++ app/test_helper.go
@@ -1,200 +1,180 @@
 package app
 
-// import (
-// 	"encoding/json"
-// 	"selfchain/app/params"
-// 	migrationtypes "selfchain/x/migration/types"
-// 	"testing"
-// 	"time"
+import (
+	"encoding/json"
+	"selfchain/app/params"
+	migrationtypes "selfchain/x/migration/types"
+	"testing"
+	"time"
 
-// 	dbm "github.com/cometbft/cometbft-db"
-// 	abci "github.com/cometbft/cometbft/abci/types"
-// 	"github.com/cometbft/cometbft/libs/log"
-// 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
-// 	tmtypes "github.com/cometbft/cometbft/types"
-// 	"github.com/cosmos/cosmos-sdk/codec"
-// 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-// 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
-// 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
-// 	"github.com/cosmos/cosmos-sdk/std"
-// 	"github.com/cosmos/cosmos-sdk/testutil/mock"
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
-// 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-// 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-// 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-// 	"github.com/stretchr/testify/require"
-// )
+	dbm "github.com/cometbft/cometbft-db"
+	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/cometbft/cometbft/libs/log"
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	tmtypes "github.com/cometbft/cometbft/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/std"
+	"github.com/cosmos/cosmos-sdk/testutil/mock"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/stretchr/testify/require"
+)
 
-// // MakeTestEncodingConfig creates an EncodingConfig for a non-amino based test configuration.
-// // This function should be used only internally (in the SDK).
-// // App user should'nt create new codecs - use the app.AppCodec instead.
-// // [DEPRECATED]
-// func MakeTestEncodingConfigAmino() params.EncodingConfig {
-// 	amino := codec.NewLegacyAmino()
-// 	interfaceRegistry := codectypes.NewInterfaceRegistry()
-// 	marshaler := codec.NewProtoCodec(interfaceRegistry)
-// 	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
+// MakeTestEncodingConfig creates an EncodingConfig for a non-amino based test configuration.
+// This function should be used only internally (in the SDK).
+// App user should'nt create new codecs - use the app.AppCodec instead.
+// [DEPRECATED]
+func MakeTestEncodingConfigAmino() params.EncodingConfig {
+	amino := codec.NewLegacyAmino()
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+	marshaler := codec.NewProtoCodec(interfaceRegistry)
+	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
 
-// 	return params.EncodingConfig{
-// 		InterfaceRegistry: interfaceRegistry,
-// 		Marshaler:         marshaler,
-// 		TxConfig:          txCfg,
-// 		Amino:             amino,
-// 	}
-// }
+	return params.EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Marshaler:         marshaler,
+		TxConfig:          txCfg,
+		Amino:             amino,
+	}
+}
 
-// func MakeTestEncodingConfig() params.EncodingConfig {
-// 	encodingConfig := MakeTestEncodingConfigAmino()
-// 	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
-// 	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-// 	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
-// 	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-// 	return encodingConfig
-// }
+func MakeTestEncodingConfig() params.EncodingConfig {
+	encodingConfig := MakeTestEncodingConfigAmino()
+	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	return encodingConfig
+}
 
-// // EmptyAppOptions is a stub implementing AppOptions
-// type EmptyAppOptions struct{}
+// EmptyAppOptions is a stub implementing AppOptions
+type EmptyAppOptions struct{}
 
-// // Get implements AppOptions
-// func (ao EmptyAppOptions) Get(o string) interface{} {
-// 	return nil
-// }
+// Get implements AppOptions
+func (ao EmptyAppOptions) Get(o string) interface{} {
+	return nil
+}
 
-// // DefaultConsensusParams defines the default Tendermint consensus params used in
-// // App testing.
-// var DefaultConsensusParams = &abci.ConsensusParams{
-// 	Block: &abci.BlockParams{
-// 		MaxBytes: 200000,
-// 		MaxGas:   2000000,
-// 	},
-// 	Evidence: &tmproto.EvidenceParams{
-// 		MaxAgeNumBlocks: 302400,
-// 		MaxAgeDuration:  504 * time.Hour, // 3 weeks is the max duration
-// 		MaxBytes:        10000,
-// 	},
-// 	Validator: &tmproto.ValidatorParams{
-// 		PubKeyTypes: []string{
-// 			tmtypes.ABCIPubKeyTypeEd25519,
-// 		},
-// 	},
-// }
+func setup(withGenesis bool, invCheckPeriod uint) (*App, GenesisState) {
+	db := dbm.NewMemDB()
+	encCdc := MakeTestEncodingConfig()
+	app := New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, DefaultNodeHome, invCheckPeriod, encCdc, EmptyAppOptions{})
+	if withGenesis {
+		return app, NewDefaultGenesisState(encCdc.Marshaler)
+	}
+	return app, GenesisState{}
+}
 
-// func setup(withGenesis bool, invCheckPeriod uint) (*App, GenesisState) {
-// 	db := dbm.NewMemDB()
-// 	encCdc := MakeTestEncodingConfig()
-// 	app := New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, DefaultNodeHome, invCheckPeriod, encCdc, EmptyAppOptions{})
-// 	if withGenesis {
-// 		return app, NewDefaultGenesisState(encCdc.Marshaler)
-// 	}
-// 	return app, GenesisState{}
-// }
+// Setup initializes a new App. A Nop logger is set in App.
+func Setup(t *testing.T, isCheckTx bool) *App {
+	t.Helper()
 
-// // Setup initializes a new App. A Nop logger is set in App.
-// func Setup(t *testing.T, isCheckTx bool) *App {
-// 	t.Helper()
+	privVal := mock.NewPV()
+	pubKey, err := privVal.GetPubKey()
+	require.NoError(t, err)
 
-// 	privVal := mock.NewPV()
-// 	pubKey, err := privVal.GetPubKey()
-// 	require.NoError(t, err)
+	// create validator set with single validator
+	validator := tmtypes.NewValidator(pubKey, 1)
+	valSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{validator})
 
-// 	// create validator set with single validator
-// 	validator := tmtypes.NewValidator(pubKey, 1)
-// 	valSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{validator})
+	// generate genesis account
+	senderPrivKey := secp256k1.GenPrivKey()
+	acc := authtypes.NewBaseAccount(senderPrivKey.PubKey().Address().Bytes(), senderPrivKey.PubKey(), 0, 0)
 
-// 	// generate genesis account
-// 	senderPrivKey := secp256k1.GenPrivKey()
-// 	acc := authtypes.NewBaseAccount(senderPrivKey.PubKey().Address().Bytes(), senderPrivKey.PubKey(), 0, 0)
+	balance := banktypes.Balance{
+		Address: acc.GetAddress().String(),
+		Coins:   sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(100000000000000))),
+	}
 
-// 	balance := banktypes.Balance{
-// 		Address: acc.GetAddress().String(),
-// 		Coins:   sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(100000000000000))),
-// 	}
+	return SetupWithGenesisValSet(t, valSet, []authtypes.GenesisAccount{acc}, balance)
+}
 
-// 	return SetupWithGenesisValSet(t, valSet, []authtypes.GenesisAccount{acc}, balance)
-// }
+// SetupWithGenesisValSet initializes a new App with a validator set and genesis accounts
+// that also act as delegators. For simplicity, each validator is bonded with a delegation
+// of one consensus engine unit (10^6) in the default token of the simapp from first genesis
+// account. A Nop logger is set in App.
+func SetupWithGenesisValSet(t *testing.T, valSet *tmtypes.ValidatorSet, genAccs []authtypes.GenesisAccount, balances ...banktypes.Balance) *App {
+	app, genesisState := setup(true, 5)
+	// set genesis accounts
+	authGenesis := authtypes.NewGenesisState(authtypes.DefaultParams(), genAccs)
+	genesisState[authtypes.ModuleName] = app.AppCodec().MustMarshalJSON(authGenesis)
 
-// // SetupWithGenesisValSet initializes a new App with a validator set and genesis accounts
-// // that also act as delegators. For simplicity, each validator is bonded with a delegation
-// // of one consensus engine unit (10^6) in the default token of the simapp from first genesis
-// // account. A Nop logger is set in App.
-// func SetupWithGenesisValSet(t *testing.T, valSet *tmtypes.ValidatorSet, genAccs []authtypes.GenesisAccount, balances ...banktypes.Balance) *App {
-// 	app, genesisState := setup(true, 5)
-// 	// set genesis accounts
-// 	authGenesis := authtypes.NewGenesisState(authtypes.DefaultParams(), genAccs)
-// 	genesisState[authtypes.ModuleName] = app.AppCodec().MustMarshalJSON(authGenesis)
+	validators := make([]stakingtypes.Validator, 0, len(valSet.Validators))
+	delegations := make([]stakingtypes.Delegation, 0, len(valSet.Validators))
 
-// 	validators := make([]stakingtypes.Validator, 0, len(valSet.Validators))
-// 	delegations := make([]stakingtypes.Delegation, 0, len(valSet.Validators))
+	bondAmt := sdk.NewInt(1000000)
 
-// 	bondAmt := sdk.NewInt(1000000)
+	for _, val := range valSet.Validators {
+		pk, err := cryptocodec.FromTmPubKeyInterface(val.PubKey)
+		require.NoError(t, err)
+		pkAny, err := codectypes.NewAnyWithValue(pk)
+		require.NoError(t, err)
+		validator := stakingtypes.Validator{
+			OperatorAddress:   sdk.ValAddress(val.Address).String(),
+			ConsensusPubkey:   pkAny,
+			Jailed:            false,
+			Status:            stakingtypes.Bonded,
+			Tokens:            bondAmt,
+			DelegatorShares:   sdk.OneDec(),
+			Description:       stakingtypes.Description{},
+			UnbondingHeight:   int64(0),
+			UnbondingTime:     time.Unix(0, 0).UTC(),
+			Commission:        stakingtypes.NewCommission(sdk.ZeroDec(), sdk.ZeroDec(), sdk.ZeroDec()),
+			MinSelfDelegation: sdk.ZeroInt(),
+		}
+		validators = append(validators, validator)
+		delegations = append(delegations, stakingtypes.NewDelegation(genAccs[0].GetAddress(), val.Address.Bytes(), sdk.OneDec()))
 
-// 	for _, val := range valSet.Validators {
-// 		pk, err := cryptocodec.FromTmPubKeyInterface(val.PubKey)
-// 		require.NoError(t, err)
-// 		pkAny, err := codectypes.NewAnyWithValue(pk)
-// 		require.NoError(t, err)
-// 		validator := stakingtypes.Validator{
-// 			OperatorAddress:   sdk.ValAddress(val.Address).String(),
-// 			ConsensusPubkey:   pkAny,
-// 			Jailed:            false,
-// 			Status:            stakingtypes.Bonded,
-// 			Tokens:            bondAmt,
-// 			DelegatorShares:   sdk.OneDec(),
-// 			Description:       stakingtypes.Description{},
-// 			UnbondingHeight:   int64(0),
-// 			UnbondingTime:     time.Unix(0, 0).UTC(),
-// 			Commission:        stakingtypes.NewCommission(sdk.ZeroDec(), sdk.ZeroDec(), sdk.ZeroDec()),
-// 			MinSelfDelegation: sdk.ZeroInt(),
-// 		}
-// 		validators = append(validators, validator)
-// 		delegations = append(delegations, stakingtypes.NewDelegation(genAccs[0].GetAddress(), val.Address.Bytes(), sdk.OneDec()))
+	}
+	// set validators and delegations
+	stakingGenesis := stakingtypes.NewGenesisState(stakingtypes.DefaultParams(), validators, delegations)
+	genesisState[stakingtypes.ModuleName] = app.AppCodec().MustMarshalJSON(stakingGenesis)
 
-// 	}
-// 	// set validators and delegations
-// 	stakingGenesis := stakingtypes.NewGenesisState(stakingtypes.DefaultParams(), validators, delegations)
-// 	genesisState[stakingtypes.ModuleName] = app.AppCodec().MustMarshalJSON(stakingGenesis)
+	totalSupply := sdk.NewCoins()
+	for _, b := range balances {
+		// add genesis acc tokens and delegated tokens to total supply
+		totalSupply = totalSupply.Add(b.Coins.Add(sdk.NewCoin(sdk.DefaultBondDenom, bondAmt))...)
+	}
 
-// 	totalSupply := sdk.NewCoins()
-// 	for _, b := range balances {
-// 		// add genesis acc tokens and delegated tokens to total supply
-// 		totalSupply = totalSupply.Add(b.Coins.Add(sdk.NewCoin(sdk.DefaultBondDenom, bondAmt))...)
-// 	}
+	// add bonded amount to bonded pool module account
+	balances = append(balances, banktypes.Balance{
+		Address: authtypes.NewModuleAddress(stakingtypes.BondedPoolName).String(),
+		Coins:   sdk.Coins{sdk.NewCoin(sdk.DefaultBondDenom, bondAmt)},
+	})
 
-// 	// add bonded amount to bonded pool module account
-// 	balances = append(balances, banktypes.Balance{
-// 		Address: authtypes.NewModuleAddress(stakingtypes.BondedPoolName).String(),
-// 		Coins:   sdk.Coins{sdk.NewCoin(sdk.DefaultBondDenom, bondAmt)},
-// 	})
+	// update total supply
+	bankGenesis := banktypes.NewGenesisState(banktypes.DefaultGenesisState().Params, balances, totalSupply, []banktypes.Metadata{}, []banktypes.SendEnabled{})
+	genesisState[banktypes.ModuleName] = app.AppCodec().MustMarshalJSON(bankGenesis)
 
-// 	// update total supply
-// 	bankGenesis := banktypes.NewGenesisState(banktypes.DefaultGenesisState().Params, balances, totalSupply, []banktypes.Metadata{})
-// 	genesisState[banktypes.ModuleName] = app.AppCodec().MustMarshalJSON(bankGenesis)
+	// checkers genesis
+	selfchainGenesis := migrationtypes.DefaultGenesis()
+	genesisState[migrationtypes.ModuleName] = app.AppCodec().MustMarshalJSON(selfchainGenesis)
 
-// 	// checkers genesis
-// 	selfchainGenesis := migrationtypes.DefaultGenesis()
-// 	genesisState[migrationtypes.ModuleName] = app.AppCodec().MustMarshalJSON(selfchainGenesis)
+	stateBytes, err := json.MarshalIndent(genesisState, "", " ")
+	require.NoError(t, err)
 
-// 	stateBytes, err := json.MarshalIndent(genesisState, "", " ")
-// 	require.NoError(t, err)
+	// init chain will set the validator set and initialize the genesis accounts
+	app.InitChain(
+		abci.RequestInitChain{
+			Validators:      []abci.ValidatorUpdate{},
+			AppStateBytes:   stateBytes,
+		},
+	)
 
-// 	// init chain will set the validator set and initialize the genesis accounts
-// 	app.InitChain(
-// 		abci.RequestInitChain{
-// 			Validators:      []abci.ValidatorUpdate{},
-// 			ConsensusParams: DefaultConsensusParams,
-// 			AppStateBytes:   stateBytes,
-// 		},
-// 	)
+	// commit genesis changes
+	app.Commit()
+	app.BeginBlock(abci.RequestBeginBlock{Header: tmproto.Header{
+		Height:             app.LastBlockHeight() + 1,
+		AppHash:            app.LastCommitID().Hash,
+		ValidatorsHash:     valSet.Hash(),
+		NextValidatorsHash: valSet.Hash(),
+	}})
 
-// 	// commit genesis changes
-// 	app.Commit()
-// 	app.BeginBlock(abci.RequestBeginBlock{Header: tmproto.Header{
-// 		Height:             app.LastBlockHeight() + 1,
-// 		AppHash:            app.LastCommitID().Hash,
-// 		ValidatorsHash:     valSet.Hash(),
-// 		NextValidatorsHash: valSet.Hash(),
-// 	}})
-
-// 	return app
-// }
+	return app
+}
--- x/migration/tests/e2e/suite_test.go
+++ x/migration/tests/e2e/suite_test.go
@@ -1,131 +1,132 @@
 // This is used for integration tests
 package test
 
-// import (
-// 	"selfchain/app"
-// 	"selfchain/x/migration"
-// 	"selfchain/x/migration/keeper"
-// 	test "selfchain/x/migration/tests"
-// 	"selfchain/x/migration/types"
-// 	"testing"
-// 	"time"
+import (
+	"selfchain/app"
+	"selfchain/x/migration"
+	"selfchain/x/migration/keeper"
+	test "selfchain/x/migration/tests"
+	"selfchain/x/migration/types"
+	"testing"
+	"time"
 
-// 	"github.com/cosmos/cosmos-sdk/baseapp"
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-// 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-// 	"github.com/stretchr/testify/suite"
-// 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-// )
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/stretchr/testify/suite"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
 
-// var (
-// 	migrationModuleAddress string
-// )
+var (
+	migrationModuleAddress string
+)
 
-// func TestMigrationTestSuite(t *testing.T) {
-// 	test.InitSDKConfig()
-// 	suite.Run(t, new(IntegrationTestSuite))
-// }
+func TestMigrationTestSuite(t *testing.T) {
+	test.InitSDKConfig()
+	suite.Run(t, new(IntegrationTestSuite))
+}
 
-// type IntegrationTestSuite struct {
-// 	suite.Suite
+type IntegrationTestSuite struct {
+	suite.Suite
 
-// 	app         *app.App
-// 	msgServer   types.MsgServer
-// 	ctx         sdk.Context
-// 	queryClient types.QueryClient
-// }
+	app         *app.App
+	msgServer   types.MsgServer
+	ctx         sdk.Context
+	queryClient types.QueryClient
+}
 
-// func (suite *IntegrationTestSuite) SetupTest() {
-// 	app := app.Setup(suite.T(), false)
-// 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now()})
+func (suite *IntegrationTestSuite) SetupTest() {
+	app := app.Setup(suite.T(), false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now()})
 
-// 	app.AccountKeeper.SetParams(ctx, authtypes.DefaultParams())
-// 	app.BankKeeper.SetParams(ctx, banktypes.DefaultParams())
-// 	migrationModuleAddress = app.AccountKeeper.GetModuleAddress(types.ModuleName).String()
+	app.AccountKeeper.SetParams(ctx, authtypes.DefaultParams())
+	app.BankKeeper.SetParams(ctx, banktypes.DefaultParams())
+	migrationModuleAddress = app.AccountKeeper.GetModuleAddress(types.ModuleName).String()
 
-// 	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
-// 	types.RegisterQueryServer(queryHelper, app.MigrationKeeper)
-// 	queryClient := types.NewQueryClient(queryHelper)
+	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelper, app.MigrationKeeper)
+	queryClient := types.NewQueryClient(queryHelper)
 
-// 	suite.app = app
-// 	suite.msgServer = keeper.NewMsgServerImpl(app.MigrationKeeper)
-// 	suite.ctx = ctx
-// 	suite.queryClient = queryClient
+	suite.app = app
+	suite.msgServer = keeper.NewMsgServerImpl(app.MigrationKeeper)
+	suite.ctx = ctx
+	suite.queryClient = queryClient
 
-// 	migration.InitGenesis(ctx, app.MigrationKeeper, getModuleGenesis())
-// }
+	migration.InitGenesis(ctx, app.MigrationKeeper, getModuleGenesis())
+}
 
-// func getModuleGenesis() types.GenesisState {
-// 	genesis := *types.DefaultGenesis()
-// 	genesis.Acl = &types.Acl{
-// 		Admin: test.AclAdmin,
-// 	}
-// 	genesis.MigratorList = []types.Migrator{
-// 		{
-// 			Migrator: test.Migrator_1,
-// 			Exists:   true,
-// 		},
-// 		{
-// 			Migrator: test.Migrator_2,
-// 			Exists:   true,
-// 		},
-// 	}
+func getModuleGenesis() types.GenesisState {
+	genesis := *types.DefaultGenesis()
+	genesis.Acl = &types.Acl{
+		Admin: test.AclAdmin,
+	}
+	genesis.MigratorList = []types.Migrator{
+		{
+			Migrator: test.Migrator_1,
+			Exists:   true,
+		},
+		{
+			Migrator: test.Migrator_2,
+			Exists:   true,
+		},
+	}
 
-// 	genesis.Config = &types.Config{
-// 		VestingDuration:    2592000, // 1 month in seconds
-// 		VestingCliff:       604800, // // 1 week in seconds
-// 		MinMigrationAmount: 2000000000000000000,
-// 	}
+	genesis.Config = &types.Config{
+		VestingDuration:    2592000, // 1 month in seconds
+		VestingCliff:       604800, // // 1 week in seconds
+		MinMigrationAmount: 2000000000000000000,
+	}
 
-// 	genesis.Params = types.Params{
-// 		HotcrossRatio: 0,
-// 	}
+	genesis.Params = types.Params{
+		HotcrossRatio: 0,
+	}
 
-// 	return genesis
-// }
+	return genesis
+}
 
-// func makeBalance(address string, balance int64, denom string) banktypes.Balance {
-// 	return banktypes.Balance{
-// 		Address: address,
-// 		Coins: sdk.Coins{
-// 			sdk.Coin{
-// 				Denom:  denom,
-// 				Amount: sdk.NewInt(balance),
-// 			},
-// 		},
-// 	}
-// }
+func makeBalance(address string, balance int64, denom string) banktypes.Balance {
+	return banktypes.Balance{
+		Address: address,
+		Coins: sdk.Coins{
+			sdk.Coin{
+				Denom:  denom,
+				Amount: sdk.NewInt(balance),
+			},
+		},
+	}
+}
 
-// func addAll(balances []banktypes.Balance) sdk.Coins {
-// 	total := sdk.NewCoins()
-// 	for _, balance := range balances {
-// 		total = total.Add(balance.Coins...)
-// 	}
-// 	return total
-// }
+func addAll(balances []banktypes.Balance) sdk.Coins {
+	total := sdk.NewCoins()
+	for _, balance := range balances {
+		total = total.Add(balance.Coins...)
+	}
+	return total
+}
 
-// func getBankGenesis() *banktypes.GenesisState {
-// 	coins := []banktypes.Balance{
-// 		makeBalance(test.Alice, 1000000000, "uslf"),
-// 		makeBalance(test.Bob, 1000000000, "uslf"),
-// 		makeBalance(test.Carol, 1000000000, "uslf"),
-// 		makeBalance(test.Migrator_1, 1000000000, "uslf"),
-// 		makeBalance(test.Migrator_2, 1000000000, "uslf"),
-// 	}
-// 	supply := banktypes.Supply{
-// 		Total: addAll(coins),
-// 	}
+func getBankGenesis() *banktypes.GenesisState {
+	coins := []banktypes.Balance{
+		makeBalance(test.Alice, 1000000000, "uslf"),
+		makeBalance(test.Bob, 1000000000, "uslf"),
+		makeBalance(test.Carol, 1000000000, "uslf"),
+		makeBalance(test.Migrator_1, 1000000000, "uslf"),
+		makeBalance(test.Migrator_2, 1000000000, "uslf"),
+	}
+	supply := banktypes.Supply{
+		Total: addAll(coins),
+	}
 
-// 	state := banktypes.NewGenesisState(
-// 		banktypes.DefaultParams(),
-// 		coins,
-// 		supply.Total,
-// 		[]banktypes.Metadata{})
+	state := banktypes.NewGenesisState(
+		banktypes.DefaultParams(),
+		coins,
+		supply.Total,
+		[]banktypes.Metadata{},
+   []banktypes.SendEnabled{})
 
-// 	return state
-// }
+	return state
+}
 
-// func (suite *IntegrationTestSuite) setupSuiteWithBalances() {
-// 	suite.app.BankKeeper.InitGenesis(suite.ctx, getBankGenesis())
-// }
+func (suite *IntegrationTestSuite) setupSuiteWithBalances() {
+	suite.app.BankKeeper.InitGenesis(suite.ctx, getBankGenesis())
+}
