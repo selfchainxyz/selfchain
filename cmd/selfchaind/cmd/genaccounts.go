05b6165de7694a5b5c3ab8d5828bd857173b5afe
Bart Simpson 7514 <bartsimpson7514@gmail.com>
2025-06-10 14:32:34 +0530

    Fix Genesis accounts can be created with invalid vesting times

--- cmd/selfchaind/cmd/genaccounts.go
+++ cmd/selfchaind/cmd/genaccounts.go
@@ -112,9 +112,18 @@ contain valid denominations. Accounts may optionally be supplied with vesting pa
 
 				switch {
 				case vestingStart != 0 && vestingEnd != 0:
+					if vestingEnd <= vestingStart {
+						return errors.New("vesting end time must be greater than start time")
+					}
+					if vestingStart < 0 || vestingEnd < 0 {
+						return errors.New("vesting times cannot be negative")
+					}
 					genAccount = authvesting.NewContinuousVestingAccountRaw(baseVestingAccount, vestingStart)
 
 				case vestingEnd != 0:
+					if vestingEnd < 0 {
+						return errors.New("vesting end time cannot be negative")
+					}
 					genAccount = authvesting.NewDelayedVestingAccountRaw(baseVestingAccount)
 
 				default:
